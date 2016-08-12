d3d00e0610ae2d247964d8071fa2917f49f33ac5
bpmurray <brendanpmurray@gmail.com>
2016-08-12 16:01:25 +0100

    Updated version numbers &started move to Makefile

--- Exactitude-tools/phase-1-get.sh
+++ Exactitude-tools/phase-1-get.sh
@@ -9,23 +9,26 @@ echo "Retrieving archives (about 30MB)"
 cd $ARCHIVEDIR
 
 # Get JAI library
-#wget -qc "http://download.java.net/media/jai/builds/release/${JAI_VERSION}/jai-${JAI_VERSION}-lib-${PLATFORMOS}-${PLATFORMVER}-jdk.bin"
 wget -c "http://download.java.net/media/jai/builds/release/${JAI_VERSION}/jai-${JAI_VERSION}-lib-${PLATFORMOS}-${PLATFORMVER}.tar.gz"
-wget -c "http://download.java.net/media/jai/builds/release/${JAI_VERSION}/jai-${JAI_MAINVERSION}-mr-doc.zip"
 
 # Get the phash dependencies - update these filenames as needed
-wget -cO CImg-${CIMG_VERSION}.zip http://sourceforge.net/projects/cimg/files/CImg-${CIMG_VERSION}.zip/download/
-wget -c http://cimg.sourceforge.net/CImg_slides.pdf
+# CImg - image + video
+wget -c http://cimg.eu/files/CImg_latest.zip
+# jpegsrc - image
 wget -c http://www.ijg.org/files/jpegsrc.v${JPEGSRC_VERSION}.tar.gz
+# ffmpeg - video
 wget -c http://ffmpeg.org/releases/ffmpeg-${FFMPEG_VERSION}.tar.gz
+# libsndfile - audio
 wget -c http://www.mega-nerd.com/libsndfile/files/libsndfile-${LIBSNDFILE_VERSION}.tar.gz
+# libsamplerate - audio
 wget -c http://www.mega-nerd.com/SRC/libsamplerate-${LIBSAMPLERATE_VERSION}.tar.gz
-wget -cO mpg123-${MPG123_VERSION}.tar.bz2 https://sourceforge.net/projects/mpg123/files/mpg123/${MPG123_VERSION}/mpg123-${MPG123_VERSION}.tar.bz2/download/
+# libmpg123 - audio
+wget -cO mpg123-${MPG123_VERSION}.tar.bz2 http://netassist.dl.sourceforge.net/project/mpg123/mpg123/${MPG123_VERSION}/mpg123-${MPG123_VERSION}.tar.bz2
 
 # Finally get pHash itself
 ### PHASH SITE IS OFFLINE!!!!
-# wget -c http://www.phash.org/releases/pHash-${PHASH_VERSION}.tar.gz
-cp ../../../../Exactitude-old/Exactitude-Dependencies/archives/pHash-${PHASH_VERSION}.tar.gz .
+wget -c http://www.phash.org/releases/pHash-${PHASH_VERSION}.tar.gz
+# cp ../../../../Exactitude-old/Exactitude-Dependencies/archives/pHash-${PHASH_VERSION}.tar.gz .
 chmod 644 *
 
 ###############################################################################
